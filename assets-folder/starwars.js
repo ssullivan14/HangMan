1170d8006ff11b45c438e23e832b16b1815b071c
sara sullivan <saralsullivan@gmail.com>
2019-10-07 17:55:27 -0400

    fully functional

--- assets-folder/starwars.js
+++ assets-folder/starwars.js
@@ -1,96 +1,124 @@
-//window.onload = function () {
+window.onload = function () {
     // Create an array of starwars words for users to guess. - global variable
     var words = ["skywalker", "tatooine", "chewbacca", "lightsaber", "skywalker", "naboo", "alderaan", "hoth"];
 
-    //global variable
-    var word = words[Math.floor(Math.random() * words.length)];
+    //global variable random word selected
+    var word;
     console.log(word);
 
     // Create a variable 
     var guesses = 12; //allowable guesses
-    var remaining = 0; //guesses remaining
     var winCounter = 0;
+    var lossCounter = 0;
 
     //document.GetElementByID has to be defined before using
     let answer = document.getElementById("answer");
     let lettersGuessed = document.getElementById("lettersGuessed");
     let winning = document.getElementById("wins");
+    let losses = document.getElementById("losses");
     let guessesRemaining = document.getElementById("guesses-remaining");
 
     var answerArray = [];
-    for (var i = 0; i < word.length; i++) {
-        answerArray[i] = "_ ";
-        answer.innerHTML += answerArray[i];
-    }
+        reset()
 
     // User inputs a letter using onkeyup.
     document.onkeyup = function (event) {
         answer.innerHTML = "";
 
-        if(!word.includes(event.key)){
-            guesses--;
-            guessesRemaining.textContent = guesses;
-        };
-
         for (var i = 0; i < word.length; i++) {
             if (word[i] == event.key) {
                 answerArray[i] = event.key;
-            } 
+            }
             answer.innerHTML += answerArray[i];
         }
 
-        if (word [i] !== event.key) {
-            answerArray[i] !== event.key;
+        //if the word is not included in the word then subtract guess and add letter to guessed letters
+        if (!word.includes(event.key)) {
+            guesses--;
+            guessesRemaining.textContent = guesses;
+            lettersGuessed.innerHTML += " " + event.key;
+        if (guesses === 0) {
+            reset()
+            lossCounter++;
+            console.log(lossCounter)
+            losses.innerHTML = lossCounter;
         }
-            lettersGuessed.innerHTML -= answerArray[i];
+        };
 
+
+        // if the word is guessed add +1 to counter else add +1 losses
         if (answer.innerHTML === word) {
             winCounter++;
             console.log(winCounter)
-            winning.innerHTML = "Wins:" + winCounter;
-        } else {
-            answer.innerHTML === 12;
-            remaining
+            winning.innerHTML = winCounter;
+            reset()
+
         }
-        console.log(answerArray[i]);
-    };
+        //  else {
+        //             lossCounter++;
+        //             console.log(lossCounter)
+        //             losses.innerHTML=lossCounter;
 
-//}
+        //         }reset();
 
+        // };
+    }
+    
+    function reset() {
+        answer.innerHTML = "";
+        lettersGuessed.innerHTML = "";
+        guesses = 12;
+        guessesRemaining.textContent = guesses;
+        word = words[Math.floor(Math.random() * words.length)];
+        answerArray = [];
+        for (var i = 0; i < word.length; i++) {
+            answerArray[i] = "_ ";
+            answer.innerHTML += answerArray[i];
+        }
 
-//What is a win
+    }
+    // function win() {
+    //     wincounter++;
+    // }
 
+    // function loss() {
 
-// Display the random word tiles.
-// var answerArray = [];
-// for (var i = 0; i < word.length; i++) {
-//     answerArray[i] = "_ ";
-//     console.log (answerArray);
-//     answer.innerHTML += answerArray[i];
-// }
-
-
-//Wins
-
-//check lettersGuessed are correct/wrong and guesses remaining
-//function checkLetter
-// document.onkeyup = function (event)
-
-//if (word[i]) != userGuess) {
-//  if (remainingGuesses === 0);
-//console.log ("Game Over");
-//}
-//  else {
-//    remainingGuesses
-// }
-
-//lettersGuessed
+    // }
 
 
+    //What is a win
 
 
-// Create a click button to start game
-// document.addEventListener("click",function(){
-//     document.getElementById ("start");
-//     console.log (start);
-//  })
+    // Display the random word tiles.
+    // var answerArray = [];
+    // for (var i = 0; i < word.length; i++) {
+    //     answerArray[i] = "_ ";
+    //     console.log (answerArray);
+    //     answer.innerHTML += answerArray[i];
+    // }
+
+
+    //Wins
+
+    //check lettersGuessed are correct/wrong and guesses remaining
+    //function checkLetter
+    // document.onkeyup = function (event)
+
+    //if (word[i]) != userGuess) {
+    //  if (remainingGuesses === 0);
+    //console.log ("Game Over");
+    //}
+    //  else {
+    //    remainingGuesses
+    // }
+
+    //lettersGuessed
+
+
+
+
+    // Create a click button to start game
+    // document.addEventListener("click",function(){
+    //     document.getElementById ("start");
+    //     console.log (start);
+}
